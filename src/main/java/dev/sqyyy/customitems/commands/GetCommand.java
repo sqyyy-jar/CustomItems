e079f5fa9c238b6e0dc670ec475d26d24c5186a7
sqyyy <62748369+sqyyy-jar@users.noreply.github.com>
2021-02-05 11:16:09 +0100

    Update GetCommand.java

--- src/main/java/dev/sqyyy/customitems/commands/GetCommand.java
+++ src/main/java/dev/sqyyy/customitems/commands/GetCommand.java
@@ -11,6 +11,7 @@ public class GetCommand implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (sender instanceof Player) {
             if (args.length == 1) {
+                if (!CustomItems.pl.newItems.contains(args[0])) return false;
                 if (CustomItems.getItem((Player)sender, args[0]) != null) ((Player) sender).getInventory().addItem(CustomItems.getItem((Player)sender, args[0]));
                 return true;
             }
