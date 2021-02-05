d0dfd129eab38a73578a849de5b6598e42c45191
sqyyy <62748369+sqyyy-jar@users.noreply.github.com>
2021-02-05 10:47:28 +0100

    Update CustomItems.java

--- src/main/java/dev/sqyyy/customitems/CustomItems.java
+++ src/main/java/dev/sqyyy/customitems/CustomItems.java
@@ -2,13 +2,21 @@ package dev.sqyyy.customitems;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import dev.sqyyy.customitems.commands.GetCommand;
 import dev.sqyyyapis.storageexplorers.GsonExplorer;
 import dev.sqyyyapis.storageexplorers.GsonFile;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeModifier;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
@@ -16,6 +24,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 public final class CustomItems extends JavaPlugin {
 
@@ -107,5 +116,76 @@ public final class CustomItems extends JavaPlugin {
                 return completions;
             }
         });
+    }
+
+    public static ItemStack getItem(Player player, String key) {
+        if (!CustomItems.pl.newItems.contains(key)) return null;
+        ItemStack item = new ItemStack(Material.AIR);
+        GsonFile file = GsonExplorer.getGson(CustomItems.pl.path + "items/" + key.toLowerCase() + ".json");
+        if (!file.has("Material")) {
+            player.sendMessage("NoMaterialException: caused by items/" + key.toLowerCase() + ".json");
+            return null;
+        }
+        try {
+            item.setType(Material.valueOf(file.get("Material").getAsString().toUpperCase()));
+        } catch (IllegalArgumentException e) {
+            player.sendMessage("IllegalArgumentException: caused by items/" + key + ".json: \"Non valid Material!\"");
+            return null;
+        }
+        ItemMeta meta = item.getItemMeta();
+        if (file.has("DisplayName")) {
+            String str = ChatColor.translateAlternateColorCodes('&', file.get("DisplayName").getAsString());
+            meta.setDisplayName("§r" + str);
+        } else {
+            if (key.length() > 0) {
+                char[] name = key.toCharArray();
+                name[0] = Character.toUpperCase(name[0]);
+                meta.setDisplayName(String.copyValueOf(name));
+            } else {
+                meta.setDisplayName("§f§lnull");
+            }
+        }
+        if (file.has("Attributes")) {
+            if (file.get("Attributes").isJsonArray()) {
+                for (JsonElement param : file.get("Attributes").getAsJsonArray()) {
+                    if (!param.isJsonObject()) continue;
+                    JsonObject object = param.getAsJsonObject();
+                    try {
+                        if (object.has("id") && object.has("name") && object.has("value")) {
+                            if (object.has("slots")) {
+                                if (object.get("slots").isJsonArray()) {
+                                    for (JsonElement param2 : object.get("slots").getAsJsonArray()) {
+                                        meta.addAttributeModifier(Attribute.valueOf(object.get("id").getAsString().toUpperCase()),
+                                                new AttributeModifier(UUID.randomUUID(), object.get("name").getAsString(),
+                                                        object.get("value").getAsDouble(), AttributeModifier.Operation.ADD_NUMBER,
+                                                        EquipmentSlot.valueOf(param2.getAsString().toUpperCase())));
+                                    }
+                                } else {
+                                    player.sendMessage("ObjectTypeException: caused by items/" + key + ".json: \"Slots must be an array!\"");
+                                }
+                            } else {
+                                meta.addAttributeModifier(Attribute.valueOf(object.get("id").getAsString().toUpperCase()),
+                                        new AttributeModifier(UUID.randomUUID(), object.get("name").getAsString(),
+                                                object.get("value").getAsDouble(), AttributeModifier.Operation.ADD_NUMBER, EquipmentSlot.HAND));
+                            }
+                        } else {
+                            player.sendMessage("IllegalArgumentException: caused by items/" + key + ".json: \"Could not find field name, value or id!\"");
+                        }
+                    } catch (IllegalArgumentException exception) {
+                        player.sendMessage("IllegalArgumentException: caused by items/" + key + ".json: \"Non valid Attribute!\"");
+                    }
+                }
+            } else {
+                player.sendMessage("ObjectTypeException: caused by items/" + key + ".json: \"Attributes must be an array!\"");
+            }
+        }
+        if (file.has("Unbreakable")) {
+            meta.setUnbreakable(file.get("Unbreakable").getAsBoolean());
+        }
+        if (file.has("CustomModelData")) {
+            meta.setCustomModelData(file.get("CustomModelData").getAsInt());
+        }
+        item.setItemMeta(meta);
+        return item;
     }
 }
